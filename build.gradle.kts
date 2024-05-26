3eabb0eaa7740a95065e01a6cbd417441e97f66f
lucid-soft <lucid-soft@github.com>
2024-05-26 09:53:31 -0400

    Sideload Version 6.4.4 - Added an equipment check fix that prevents plugins from erroring out if you are wearing an invalid item like the Soul Wars Icon glitched beard slot item.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 }
 
 group = "com.lucidplugins"
-version = "6.4.3"
+version = "6.4.4"
 
 val javaMajorVersion = JavaVersion.VERSION_11.majorVersion
 
--- src/main/java/com/lucidplugins/api/utils/Equipment.java
+++ src/main/java/com/lucidplugins/api/utils/Equipment.java
@@ -65,6 +65,12 @@ public class Equipment {
                 if (item.getId() == 6512 || item.getId() == -1) {
                     continue;
                 }
+
+                if (equipmentSlotWidgetMapping.get(i) == null)
+                {
+                    continue;
+                }
+
                 Widget w = client.getWidget(WidgetInfo.EQUIPMENT.getGroupId(), equipmentSlotWidgetMapping.get(i));
                 if (w == null || w.getActions() == null) {
                     continue;
